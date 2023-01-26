2a8a8144cef856e3223fe599453ad1ee131c0796
Charlie Francesconi <m251806@usna.edu>
2023-01-25 22:32:18 -0500

    finished comments on Formatter.java

--- Formatter.java
+++ Formatter.java
@@ -1,9 +1,23 @@
 /**
- * Used to take a String array and prints the array to termian
+ * Prints an array Strings to console given a limited number of columns per 
+ * line. If a String exceeds the column limint for that line it is printed on  
+ * the next line.
  * @author Charlie Francesconi
  */
+
 import java.util.*;
+
+/**
+ * Prints a string array to console given a limit of columns per line.
+ */
 public class Formatter {
+    /**
+     * Prints String array A to console given limit cols per line. If a String
+     * in the array will exceed the current line limit of cols then it
+     * is printed on a new line.
+     * @param A String array with Strings to be printed to the console
+     * @param cols Number of characters per line
+     */
     public static void writeInColumns(String[] A, int cols) {
         int lineTotal = 0;
         for(int i = A.length - 1; i >= 0; i--) {
@@ -17,6 +31,10 @@ public class Formatter {
         System.out.println();
     }
 
+    /**
+     * Debugging main method for testing get() method.
+     * @param Args Default
+     */
     public static void main(String [] Args) {
         Scanner in = new Scanner(System.in);
         String [] words = {"These", "are", "the", "times", "that", "try", "men's", "souls."};
--- ListStuff.java
+++ ListStuff.java
@@ -4,7 +4,8 @@
  */
 
 public class ListStuff {
-  // addToFront(s,Nold) returns a StringNode reference representing the list obtained by adding s to the front of list Nold
+  // addToFront(s,Nold) returns a StringNode reference representing the list
+  // obtained by adding s to the front of list Nold
   public static StringNode addToFront(String s, StringNode Nold) {
     StringNode front = new StringNode();
     front.data = s;
@@ -12,7 +13,8 @@ public class ListStuff {
     return front;
   }
 
-  // listToArray(N) returns a reference to an array containing the same strings as in the list N (in the order they were received)
+  // listToArray(N) returns a reference to an array containing the same
+  // strings as in the list N (in the order they were received)
   public static String[] listToArray(StringNode N) {
     int length = ListLen(N);
     String [] A = new String[length];
@@ -31,10 +33,10 @@ public class ListStuff {
   }
 
   public static void main(String [] Args) {
-    StringNode N = null;      // at this point N *is* an empty list
-    N = addToFront("rat",N);  // at this point N *is* the list ("rat")
-    N = addToFront("dog",N);  // at this point N *is* the list ("dog","rat")
-    N = addToFront("pig",N);  // at this point N *is* the list ("pig","dog","rat")
+    StringNode N = null;
+    N = addToFront("rat",N);
+    N = addToFront("dog",N);  
+    N = addToFront("pig",N);  
     String[] A = listToArray(N);
     for(int i = 0; i < A.length; i++) {
       System.out.println(A[i]);
