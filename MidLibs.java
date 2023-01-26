224828214103759cb8e7f9921acfcad5ac39fff8
Charlie Francesconi <m251806@usna.edu>
2023-01-25 22:01:38 -0500

    i dont even know

--- MidLibs.java
+++ MidLibs.java
@@ -2,5 +2,6 @@ public class MidLibs{
     public static void main(String [] args){
         String[] madLib = WordRead.get(args[0]);
         Formatter.writeInColumns(madLib, 35);
+        
     }
 }
